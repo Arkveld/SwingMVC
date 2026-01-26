b81c45661a9c104fc175d1202e73b616ad210b21
Michael <michael.mabonzo@gmail.com>
2026-01-26 14:43:11 +0100

    Ajout de la Dao dans RegisterController

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import dao.AccountDao;
 import model.Account;
 import model.Customer;
 import view.ViewHome;
@@ -19,17 +20,33 @@ public class RegisterController {
         });
 
         viewRegister.addButtonRegister(e-> {
+
+            //Récupère les données du formulaire
             String firstname = viewRegister.getInputFirstname();
             String lastname = viewRegister.getInputlastName();
             String email = viewRegister.getInputEmail();
             String password = viewRegister.getInputPassword();
 
-            //Instance d'un client
-            Customer customer = new Customer(firstname, lastname, email, password);
-            //Instance d'un account
-            Account account = new Account(customer);
-            viewRegister.viewMessage(account.getCustomer().getFirstname());
+            //Vérifiz si tous les champs sont remplis
+            if(firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || password.isEmpty()){
+                viewRegister.viewMessage("Veuillez remplir tous les champs du formulaire");
+            } else {
+                //Instance d'un client
+                Customer customer = new Customer(firstname, lastname, email, password);
+                //Instance d'un account
+                Account account = new Account(customer);
+                //Instance de la dao
+                AccountDao accountDao = new AccountDao();
+                //Envoi vers la BDD
+                if(accountDao.register(account)){
+                    viewRegister.viewMessage("Inscription réussie");
+                    //Redirection
+                    viewRegister.closeView();
+                    new HomeController(new ViewHome());
+                } else {
+                    viewRegister.viewMessage("Échec de lors l'inscription");
+                }
+            }
         });
     }
-
 }
