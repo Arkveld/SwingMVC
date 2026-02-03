1720be30576bb37589fb1ef07ecbed2862ae3025
Michael <michael.mabonzo@gmail.com>
2026-02-03 14:15:30 +0100

    SplashController crée

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,15 @@
 package app;
 
 import controller.HomeController;
+import controller.SplashController;
 import view.ViewHome;
+import view.ViewSplash;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        new SplashController(new ViewSplash());
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
---
+++ src/controller/SplashController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import view.ViewSplash;
+
+public class SplashController {
+    private ViewSplash viewSplash;
+
+    public SplashController(ViewSplash viewSplash) {
+        this.viewSplash = viewSplash;
+    }
+}
